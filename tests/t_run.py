dfe605d5bbe7831b9f6404e90b866ddba1f0259f
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-04-16 22:37:47 +0200

    tests: Scheduled app stop to exit test

--- tests/t_run.py
+++ tests/t_run.py
@@ -1,14 +1,20 @@
 import time
 
 from holytools.devtools import Unittest
+from kivy.clock import Clock
+
 from filebundler.app import DataCollectApp
 
 
 class TestAppLaunch(Unittest):
     def test_launch(self):
+        def stop_app(*args):
+            app.stop()
+
+        Clock.schedule_once(stop_app, 5)
+
         app = DataCollectApp()
         app.run()
-        time.sleep(20)
 
 if __name__ == "__main__":
     TestAppLaunch.execute_all()
