ae7d8f1c1144cf5e2a49840c137a755b81ba87b0
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-04-16 22:31:21 +0200

    run: Updated development targets dirs

--- filebundler/run/dev_run.py
+++ filebundler/run/dev_run.py
@@ -13,14 +13,14 @@ def main():
     win_test_dir = 'C:\\Users\\daniel\\OneDrive\\Desktop\\test_folder'
     win_target_test = 'C:\\Users\\daniel\\OneDrive\\Desktop'
 
-    linux_input_test = '/home/daniel/OneDrive'
-    linux_target_test = f'/home/daniel/aimat/tool_target_folder'
+    linux_input_test = '/home/daniel/testdir/sourcefiles'
+    linux_target_test = '/home/daniel/testdir'
 
 
     _, __ = win_test_dir, win_target_test
     _, __ = linux_input_test, linux_target_test
 
-    app = DataCollectApp(override_input_folder=None,
+    app = DataCollectApp(override_input_folder=linux_target_test,
                          override_target_folder=linux_target_test)
     inspector.create_inspector(Window, app)
 
