6a534bf5193e30dc1696da9ac37624da7fbcd5d1
Daniel Hollarek <daniel.hollarek@googlemail.com>
2025-04-16 22:09:25 +0200

    Interval: Implemented interval subtract

--- filebundler/resources/interval.py
+++ filebundler/resources/interval.py
@@ -8,14 +8,26 @@ class Interval:
     upper : float
 
     def subtract(self, b : Interval) -> list[Interval]:
-        a = self
-        if b.lower <= a.lower:
-            if b.upper < a.upper:
-                return [Interval(b.upper, a.upper)]
+        a1, a2 = self.lower, self.upper
+        b1, b2 = b.lower, b.upper
+
+        if b1 <= a1:
+            if b2 < a1:
+                return [Interval(a1, a2)]
+            elif b2 < a2:
+                return [Interval(b2, a2)]
             else:
                 return []
-        else:
-            if b.upper < a.lower:
-                return [Interval(a.lower, b.lower), Interval(b.upper, a.upper)]
+
+        elif a1 < b1 < a2:
+            if b2 < a2:
+                return [Interval(a1, b1), Interval(b2, a2)]
             else:
-                return [Interval(a.lower, b.lower)]
+                return [Interval(a1,b1)]
+
+        else:
+            return [Interval(a1,a2)]
+
+
+    def __eq__(self, other):
+        return self.lower == other.lower and self.upper == other.upper
--- tests/t_interval.py
+++ tests/t_interval.py
@@ -8,32 +8,57 @@ class TestIntervalSubtraction(Unittest):
     def test_no_overlap(self):
         a = Interval(1, 5)
         b = Interval(6, 10)
-        self.assertEqual(a.subtract(b), [a])
+
+        diff = a.subtract(b)
+        print(diff)
+
+        self.assertTrue(len(diff) == 1)
+        i1 = a.subtract(b)[0]
+        self.assertTrue(i1 == Interval(1, 5))
 
     def test_overlap_lower(self):
         a = Interval(1, 5)
         b = Interval(0, 3)
-        self.assertEqual(a.subtract(b), [Interval(3, 5)])
+
+        diff = a.subtract(b)
+        print(diff)
+        self.assertTrue(len(diff) == 1)
+        i1 = a.subtract(b)[0]
+        self.assertTrue(i1 == Interval(3, 5))
 
     def test_overlap_upper(self):
         a = Interval(1, 5)
         b = Interval(3, 6)
-        self.assertEqual(a.subtract(b), [Interval(1, 3)])
+
+        i1 = a.subtract(b)[0]
+        self.assertTrue(len(a.subtract(b)) == 1)
+        self.assertTrue(i1 == Interval(1, 3))
 
     def test_overlap_inside(self):
         a = Interval(1, 5)
         b = Interval(2, 4)
-        self.assertEqual(a.subtract(b), [Interval(1, 2), Interval(4, 5)])
+
+        intervals = a.subtract(b)
+        print(f'{intervals}')
+
+        self.assertTrue(len(intervals) == 2)
+        i1, i2 = intervals[0], intervals[1]
+        self.assertTrue(i1 == Interval(1, 2))
+        self.assertTrue(i2 == Interval(4, 5))
 
     def test_entire_coverage(self):
         a = Interval(1, 5)
         b = Interval(0, 6)
-        self.assertEqual(a.subtract(b), [])
+        self.assertTrue(len(a.subtract(b)) == 0)
+
 
     def test_exact_overlap(self):
         a = Interval(1, 5)
         b = Interval(1, 5)
-        self.assertEqual(a.subtract(b), [])
+
+        diff = a.subtract(b)
+        print(diff)
+        self.assertTrue(len(diff) == 0)
 
 
 if __name__ == '__main__':
